1d90099e175001854c155729835f0c16e96bfc3c
aqifbashah <aqifbashah@gmail.com>
2024-02-15 19:48:09 +0800

    add env for deployment

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,11 +1,12 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-  host: "localhost",
-  user: "postgres",
-  password: "admin",
-  database: "url_shortener",
-  port: "5432",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  ssl: process.env.DB_SSL,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
